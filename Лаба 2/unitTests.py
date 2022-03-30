8fab19824cfa8158bd03b0854f36f551b7d1f908
lubalu999 <85310638+lubalu999@users.noreply.github.com>
2022-03-30 11:32:44 +0300

    Update unitTests.py

--- Лаба 2/unitTests.py
+++ Лаба 2/unitTests.py
@@ -1,4 +1,4 @@
-from NFA import *
+from Auto import *
 import unittest
 
 class Test1(unittest.TestCase):
@@ -10,10 +10,10 @@ class Test1(unittest.TestCase):
 
     def test_string2(self):
         self.assertEqual(set(self.auto.FindAll('мылараму')), set(['мыла','раму']))
-#
+        
     def test_string3(self):
         self.assertEqual(set(self.auto.FindAll('мамамылараму')), set(['мыла','раму','мама']))
-#
+
     def test_string4(self):
         self.assertEqual(self.auto.FindAll('ПАПАПОЧИСТИЛ'), [])
 
@@ -28,11 +28,9 @@ class Test2(unittest.TestCase):
     def test_string2(self):
         self.assertEqual(set(self.auto.FindAll('abac')), set(['ac', 'abac']))
 
-    #
     def test_string3(self):
         self.assertEqual(self.auto.FindAll('bbbbcccc'), [])
 
-    #
     def test_string4(self):
         self.assertEqual(self.auto.FindAll('aaac'), ['aaac','aac','ac'])
 
